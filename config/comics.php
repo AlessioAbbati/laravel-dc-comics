a57273449f570d9e61a835f76560ed0c95508352
AlessioAbbati <alex_117@live.it>
2023-06-29 13:48:49 +0200

    add laravel base and array with six comics

---
+++ config/comics.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+    [
+        "src" => "batman1.jpg",
+        "title" => "batman",
+        "type" => "action",
+        "author" => " Bill Finger",
+        "issue_number" => 1,
+        "year" => 1994,
+        "description" => "batman never die"
+    ],
+    [
+        "src" => "flash1.jpg",
+        "title" => "flash",
+        "type" => "action",
+        "author" => " Bill Finger",
+        "issue_number" => 5,
+        "year" => 1994,
+        "description" => "flash run"
+    ],
+    [
+        "src" => "greenLantern1.jpg",
+        "title" => "green lantern",
+        "type" => "sci-fi",
+        "author" => " Bill Finger",
+        "issue_number" => 77,
+        "year" => 1994,
+        "description" => "green lantern is green WOW!!!"
+    ],
+    [
+        "src" => "joker1.jpg",
+        "title" => "joker",
+        "type" => "madness",
+        "author" => " Bill Finger",
+        "issue_number" => 93,
+        "year" => 1994,
+        "description" => "Never dance with the devil in the pale full moon"
+    ],
+    [
+        "src" => "spiderman1.jpg",
+        "title" => "spider-man",
+        "type" => "action",
+        "author" => " Bill Finger",
+        "issue_number" => 33,
+        "year" => 1994,
+        "description" => "who killed spider-man???"
+    ],
+    [
+        "src" => "superman1.jpg",
+        "title" => "superman",
+        "type" => "action",
+        "author" => " Bill Finger",
+        "issue_number" => 15,
+        "year" => 1994,
+        "description" => "criptonite is green"
+    ],
+];    
---
+++ database/migrations/2023_06_29_112137_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("src", 200);
+            $table->string("title", 100);
+            $table->string("type", 20);
+            $table->string("author", 100);
+            $table->integer("issue_number");
+            $table->integer("year");
+            $table->text("description");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
