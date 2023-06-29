ed365a00f5b094808b8d6b132c6393c3ad1a3933
AlessioAbbati <alex_117@live.it>
2023-06-29 17:57:06 +0200

    mod style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,24 +2,31 @@
 
 @section('contents')
 
-@foreach ($comics as $comic)
-    <div class="card">
-        <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">{{ $comic->title }}</h5>
-            {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">{{ $comic->price }}</li>
-            <li class="list-group-item">{{ $comic->series }}</li>
-            <li class="list-group-item">{{ $comic->type }}</li>
-            <li class="list-group-item">{{ $comic->sale_date }}</li>
-        </ul>
-        <div class="card-body">
-            <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-primary">Comic info</a>
-            <a href="{{ route('comics.create', ['comic' => $comic->id]) }}" class="btn btn-primary">Another link</a>
-        </div>
+<div class="container">
+    <div class="row row-cols-3">
+        @foreach ($comics as $comic)
+            <div class="col">
+                <div class="card h-100">
+                    <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $comic->title }}</h5>
+                        {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{{ $comic->price }}</li>
+                        <li class="list-group-item">{{ $comic->series }}</li>
+                        <li class="list-group-item">{{ $comic->type }}</li>
+                        <li class="list-group-item">{{ $comic->sale_date }}</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-primary">Comic info</a>
+                        <a href="{{ route('comics.create', ['comic' => $comic->id]) }}" class="btn btn-primary">Another link</a>
+                    </div>
+                </div>
+            </div>
+    
+        @endforeach
     </div>
-@endforeach
+</div>
 
 @endsection
