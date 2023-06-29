8239889392b06432aa98a1062a9c4eebb40e0208
AlessioAbbati <alex_117@live.it>
2023-06-29 17:20:24 +0200

    add index with foreach

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Guest\ComicController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 
