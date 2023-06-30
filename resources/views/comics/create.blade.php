82300a24a6f78e26b59ace3cf984a55d91790e60
AlessioAbbati <alex_117@live.it>
2023-06-30 15:21:43 +0200

    mod create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,39 +3,93 @@
 @section('contents')
 
 <h1>Add new comic card</h1>
-    <form method="POST" action="{{ route('comics.store') }}">
-        <div class="mb-3">
-            <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title">
+
+<form method="POST" action="{{ route('pastas.store') }}">
+    @csrf
+
+    <div class="mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input
+            type="text"
+            class="form-control @error('title') is-invalid @enderror"
+            id="title"
+            name="title"
+            value="{{ old('title') }}"
+        >
+        <div class="invalid-feedback">
+            @error('title') {{ $message }} @enderror
         </div>
+    </div>
 
-        <div class="mb-3">
-            <label for="thumb" class="form-label">thumb</label>
-            <input type="text" class="form-control" id="thumb">
+    <div class="mb-3">
+        <label for="thumb" class="form-label">Immagine</label>
+        <input
+            type="text"
+            class="form-control @error('thumb') is-invalid @enderror"
+            id="thumb"
+            name="thumb"
+            value="{{ old('thumb') }}"
+        >
+        <div class="invalid-feedback">
+            @error('thumb') {{ $message }} @enderror
         </div>
+    </div>
 
-        <div class="mb-3">
-            <label for="type" class="form-label">type</label>
-            <input type="text" class="form-control" id="type">
+    <div class="mb-3">
+        <label for="type" class="form-label">Type</label>
+        <input
+            type="text"
+            class="form-control @error('type') is-invalid @enderror"
+            id="type"
+            name="type"
+            value="{{ old('type') }}"
+        >
+        <div class="invalid-feedback">
+            @error('type') {{ $message }} @enderror
         </div>
+    </div>
 
-        <div class="mb-3">
-            <label for="series" class="form-label">series</label>
-            <input type="text" class="form-control" id="series">
+    <div class="mb-3">
+        <label for="series" class="form-label">series</label>
+        <input
+            type="text"
+            class="form-control @error('series') is-invalid @enderror"
+            id="series"
+            name="series"
+            value="{{ old('series') }}"
+        >
+        <div class="invalid-feedback">
+            @error('series') {{ $message }} @enderror
         </div>
+    </div>
 
-        <div class="mb-3">
-            <label for="price" class="form-label">price</label>
-            <input type="text" class="form-control" id="price">
+    <div class="mb-3">
+        <label for="price" class="form-label">Price</label>
+        <input
+            type="text"
+            class="form-control @error('price') is-invalid @enderror"
+            id="price"
+            name="price"
+            value="{{ old('price') }}"
+        >
+        <div class="invalid-feedback">
+            @error('price') {{ $message }} @enderror
         </div>
+    </div>
 
-
-        <div class="mb-3">
-            <label for="description" class="form-label">description</label>
-            <textarea class="form-control" id="description" rows="3"></textarea>
+    <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea
+            class="form-control @error('description') is-invalid @enderror"
+            id="description"
+            rows="3"
+            name="description">{{ old('description') }}</textarea>
+        <div class="invalid-feedback">
+            @error('description') {{ $message }} @enderror
         </div>
+    </div>
 
-        <button class="btn btn-primary">Submit</button>
-    </form>
+    <button class="btn btn-primary">Salva</button>
+</form>
 
 @endsection
