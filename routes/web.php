3c0a5eb8d0140649ec7bd9d690e4c68c98dcec5e
AlessioAbbati <alex_117@live.it>
2023-07-03 15:36:09 +0200

    add route to trashed

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\Guest\ComicController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 
-
+route::get('/comics/trashed', [PastaController::class, 'trashed'])->name('comics.trashed');
 route::resource('comics', ComicController::class);
