d5381705bf11007cfb864699fa317b4d270339d9
AlessioAbbati <alex_117@live.it>
2023-07-03 16:25:20 +0200

    mod style navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,9 +1,9 @@
-    <div class="nav d-flex justify-content-between">
+    <div class="nav d-flex justify-content-between align-items-center">
         <h1 class="text-center">Comics</h1>
         <div class="controls">
             <a class="btn btn-primary" href="{{ route('comics.index') }}">to the comics</a>
             <a class="btn btn-primary" href="{{ route('home') }}">home</a>
             <a class="btn btn-primary" href="{{ route('comics.create') }}">New card</a>
-            <a class="btn btn-primary" href="{{ route('comics.trashed') }}">cestino</a>
+            <a class="btn btn-primary" href="{{ route('comics.trashed') }}">Bin</a>
         </div>
     </div>
